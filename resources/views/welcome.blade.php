91e3174cf5a3cba6c83cf18c044faa0a746d55e0
Yann Boyongo <yaboyon3@gmail.com>
2023-01-14 08:07:26 +0200

    adding posts Model, creating post componenets and updating the welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,28 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Livewire Crud</title>
+    <link data-minify="1"
+        href="https://www.bacancytechnology.com/blog/wp-content/cache/min/1/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css?ver=1672663261"
+        rel="stylesheet" crossorigin="anonymous">
     @livewireStyles
 </head>
 
-<body class="antialiased">
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Livewire CRUD Blog</a>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            @livewire('post')
+        </div>
+    </div>
 
     @livewireScripts
 </body>
